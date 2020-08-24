908319e6b741bcb041364f948d23fb7000eaa3e3
jinji601060533 <yjs601060533@qq.com>
2020-08-24 10:56:20 +0800

    构建项目，配置全局样式，路由，element-ui

---
+++ src/views/Login/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="login">
+        这里是登录页面
+         <el-button>默认按钮</el-button>
+        </div>
+</template>
+<script>
+</script>
+<style lang="scss" scoped>
+#login{
+    //vh是可视区百分比单位
+    height: 100vh;
+    background-color: #344a5f;
+}
+</style>>
